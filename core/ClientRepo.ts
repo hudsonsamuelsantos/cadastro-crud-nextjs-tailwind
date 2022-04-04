3028a3df80ee6558cb2bd6c0bf6b35bba3bb6396
Hudson Santos <hudsnss@gmail.com>
2022-04-04 06:24:46 -0300

    update

--- core/ClientRepo.ts
+++ core/ClientRepo.ts
@@ -3,5 +3,5 @@ import Client from "./Client";
 export default interface ClientRepo {
     save(client: Client): Promise<Client>
     delete(client: Client): Promise<void>
-    getAll(): Promise<Client>
+    getAll(): Promise<Client[]>
 }
